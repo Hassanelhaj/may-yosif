bf5ff2b13dab4ef41bf335d3dce68ee75839c37a
hassan <hassangnsa19@gmail.com>
2024-03-29 02:33:28 -0700

    add whole app

---
+++ Components/contact/SocialIcons.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BsFacebook, BsInstagram} from 'react-icons/bs'
+
+import {FaLinkedin} from 'react-icons/fa';
+import {SiGmail} from 'react-icons/si'
+
+    const SocialIcons = () => {
+        return (
+          <div className='flex justify-center items-center gap-2'>
+             <a href='https://www.facebook.com/mai.shandi.1' target='_blank' className='text-3xl hover:opacity-70' >
+              <BsFacebook/>
+              </a> 
+              <a href='https://www.instagram.com/maishandi?igsh=MWo5NmM0eDJzbThvdQ=='
+              target='_blank' className='text-3xl hover:opacity-70' >
+              <BsInstagram/>
+              </a> 
+
+     
+          
+             <a target='_blank' href='mailto:mybwsf5@gmail.com' className='text-3xl hover:opacity-70'>
+              <SiGmail/>
+              </a>
+          </div>
+        )
+      }
+
+export default SocialIcons
---
+++ Data.js
@@ -0,0 +1,26 @@
+import { TfiWrite } from "react-icons/tfi";
+import Image from "next/image";
+import serviceDesign from "./public/imgs/services/mediaDesign.svg";
+import { GiEarthAmerica, GiPaper, GiTravelDress } from "react-icons/gi";
+import { AiOutlineAppstoreAdd } from "react-icons/ai";
+import { RiTravestiFill } from "react-icons/ri";
+import { FaDochub, FaPassport, FaPlane } from "react-icons/fa";
+import { BiDockBottom, BiPaperPlane } from "react-icons/bi";
+import { FiPaperclip } from "react-icons/fi";
+import { SiPaperspace } from "react-icons/si";
+
+
+
+export const servicesList = [
+
+  {
+    id: 1,
+    title: "Content Packages ",
+    description:
+      "Profishinal Work With Social Media Manegement Services",
+    logo: <TfiWrite />,
+  },
+ 
+];
+
+
